3b55018a7d3399be2f70186dd921827b8b46f711
caik <caik@esensoft.com>
2020-12-23 17:40:41 +0800

    map

---
+++ src/main/java/spark/operations/Common.java
@@ -0,0 +1,23 @@
+package main.java.spark.operations;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+public class Common {
+
+    private static final String TEXT_FILE_PATH = "e:\\spark\\info.txt";
+
+    public static JavaSparkContext getSc() {
+        SparkSession sparkSession = SparkSession.builder().appName(Thread.currentThread().getName()).master("local").getOrCreate();
+        return new JavaSparkContext(sparkSession.sparkContext());
+    }
+
+    public static JavaRDD<String> textFile(JavaSparkContext sparkContext){
+        return sparkContext.textFile(TEXT_FILE_PATH);
+    }
+
+    public static void println(Object obj) {
+        System.out.println(String.valueOf(obj));
+    }
+}
---
+++ src/main/java/spark/operations/Map.java
@@ -0,0 +1,24 @@
+package main.java.spark.operations;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class Map {
+    public static void main(String[] args) throws InterruptedException {
+        JavaSparkContext sc = Common.getSc();
+        JavaRDD<String> javaRDD = Common.textFile(sc);
+
+        JavaRDD<String> mapedRdd = javaRDD.map(Map::map);
+
+        mapedRdd.foreach(Common::println);
+
+        sc.close();
+    }
+
+    private static String map(String line) {
+        String[] cs = line.split(",");
+        return cs[2] + "-" + cs[4] + "-" + cs[1] + "-" + cs[3];
+    }
+
+}
